2df69e25a3a24a0d2d5699b79da73857ea0704ee
TsvetanNikolov123 <tsdman1985@gmail.com>
2017-10-23 17:28:36 +0300

    Programming Fundamentals

---
+++ 23 String And Text Processing - Lab/01.ReverseString/ReverseString.cs
@@ -0,0 +1,26 @@
+﻿namespace _01.ReverseString
+{
+    using System;
+    using System.Text;
+
+    public class ReverseString
+    {
+        public static void Main()
+        {
+            string input = Console.ReadLine();
+            string reversed = ReverseStringMethod(input);
+            Console.WriteLine(reversed);
+        }
+
+        public static string ReverseStringMethod(string str)
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = str.Length - 1; i >= 0; i--)
+            {
+                sb.Append(str[i]);
+            }
+            return sb.ToString();
+        }
+
+    }
+}
