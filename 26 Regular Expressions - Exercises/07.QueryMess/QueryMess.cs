0354b3dcdb3a5c32badb207157ee62dea70bb1ec
TsvetanNikolov123 <tsdman1985@gmail.com>
2017-10-29 16:02:51 +0200

    Programming Fundamentals

---
+++ 26 Regular Expressions - Exercises/07.QueryMess/QueryMess.cs
@@ -0,0 +1,47 @@
+﻿namespace _07.QueryMess
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text.RegularExpressions;
+
+    public class QueryMess
+    {
+        public static void Main()
+        {
+            string firstRegex = @"([^&?]+)=([^&?]+)";
+            string secondRegex = @"(%20|\+)+";
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "END") break;
+
+                MatchCollection matches = Regex.Matches(input, firstRegex);
+                Dictionary<string, List<string>> dic = new Dictionary<string, List<string>>();
+
+                for (int i = 0; i < matches.Count; i++)
+                {
+                    string field = matches[i].Groups[1].Value;
+                    field = Regex.Replace(field, secondRegex, word => " ").Trim();
+
+                    string value = matches[i].Groups[2].Value;
+                    value = Regex.Replace(value, secondRegex, word => " ").Trim();
+
+                    if (!dic.ContainsKey(field))
+                    {
+                        dic[field] = new List<string>();
+                    }
+                    dic[field].Add(value);
+                }
+
+                foreach (KeyValuePair<string,List<string>> result in dic)
+                {
+                    Console.Write("{0}=[{1}]",
+                        result.Key,
+                        string.Join(", ", result.Value));
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ 26 Regular Expressions - Exercises/08.UseYourChainsBuddy/UseYourChainsBuddy.cs
@@ -0,0 +1,59 @@
+﻿namespace _08.UseYourChainsBuddy
+{
+    using System;
+    using System.Linq;
+    using System.Text;
+    using System.Text.RegularExpressions;
+
+    public class UseYourChainsBuddy
+    {
+        public static void Main()
+        {
+            Regex paragrafRegex = new Regex(@"<p>(?<message>.+?)<\/p>");
+            string text = Console.ReadLine();
+
+            string[] paragraphs = paragrafRegex.Matches(text)
+                .Cast<Match>()
+                .Select(a => a.Groups["message"].Value)
+                .Select(a => Regex.Replace(a, @"[^a-z\d]", " "))
+                .Select(a => Regex.Replace(a, @"\s+", " "))
+                .ToArray();
+
+            for (int i = 0; i < paragraphs.Length; i++)
+            {
+                paragraphs[i] = Rot13String(paragraphs[i]);
+            }
+
+            StringBuilder result = new StringBuilder();
+            foreach (string paragraph in paragraphs)
+            {
+                result.Append(paragraph);
+            }
+
+            Console.WriteLine(result.ToString());
+        }
+
+        private static string Rot13String(string input)
+        {
+            StringBuilder result = new StringBuilder();
+            foreach (char letter in input)
+            {
+                result.Append(Rot13(letter));
+            }
+            return result.ToString();
+        }
+
+        private static char Rot13(char letter)
+        {
+            if (!char.IsLetter(letter))
+            {
+                return letter;
+            }
+
+            char offset = char.IsUpper(letter) ? 'A' : 'a';
+            char rotatedLetter =(char) ((letter - offset + 13) % 26 + offset);
+
+            return rotatedLetter;
+        }
+    }
+}
