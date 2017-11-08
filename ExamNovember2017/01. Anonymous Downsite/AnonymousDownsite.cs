20040e99bf70c697c8b15f48ba20baceebedadee
TsvetanNikolov123 <tsdman1985@gmail.com>
2017-11-08 14:52:09 +0200

    exam

---
+++ ExamNovember2017/01. Anonymous Downsite/AnonymousDownsite.cs
@@ -0,0 +1,45 @@
+﻿namespace _01.Anonymous_Downsite
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Numerics;
+
+    public class AnonymousDownsite
+    {
+        public static void Main()
+        {
+            decimal n = decimal.Parse(Console.ReadLine());
+            BigInteger securityKey = BigInteger.Parse(Console.ReadLine());
+            decimal siteLoss = 0.0m;
+            
+            List<string> siteNames = new List<string>();
+
+            BigInteger sum = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                sum *= securityKey;
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] inputTokens = Console.ReadLine()
+                    .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+                string siteName = inputTokens[0];
+                siteNames.Add(siteName);
+                decimal siteVisits = decimal.Parse(inputTokens[1]);
+                decimal siteCommercialPricePerVisit = decimal.Parse(inputTokens[2]);
+
+                siteLoss += siteVisits * siteCommercialPricePerVisit;
+            }
+            foreach (var site in siteNames)
+            {
+                Console.WriteLine($"{site}");
+            }
+
+            Console.WriteLine($"Total Loss: {siteLoss:f20}");
+
+            Console.WriteLine($"Security Token: {sum}");
+        }
+    }
+}
---
+++ ExamNovember2017/04.AnonymousCache/AnonymousCache.cs
@@ -0,0 +1,104 @@
+﻿namespace _04.AnonymousCache
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+
+    public class AnonymousCache
+    {
+        public static void Main()
+        {
+            Dictionary<string, Dictionary<string, long>> dic =
+                new Dictionary<string, Dictionary<string, long>>();
+
+            Dictionary<string, Dictionary<string, long>> dicTemp =
+                new Dictionary<string, Dictionary<string, long>>();
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "thetinggoesskrra")
+                {
+                    break;
+                }
+
+                string[] tokens = input.Split(new[] { ' ', '-', '>', '|' }, StringSplitOptions.RemoveEmptyEntries);
+
+                if (tokens.Length != 1)
+                {
+                    string dataKey = tokens[0];
+                    long dataSize = long.Parse(tokens[1]);
+                    string dataSets = tokens[2];
+
+                    if (!dic.ContainsKey(dataSets))
+                    {
+                        if (!dicTemp.ContainsKey(dataSets))
+                        {
+                            dicTemp[dataSets] = new Dictionary<string, long>();
+                        }
+                        dicTemp[dataSets][dataKey] = dataSize;
+                    }
+
+                    if (dic.ContainsKey(dataSets))
+                    {
+                        dic[dataSets][dataKey] = dataSize;
+                    }
+                }
+                else
+                {
+                    string dataSet = tokens[0];
+                    if (!dic.ContainsKey(dataSet))
+                    {
+                        dic[dataSet] = new Dictionary<string, long>();
+                        if (dicTemp.ContainsKey(dataSet))
+                        {
+                            foreach (var dataset in dicTemp[dataSet])
+                            {
+
+                                dic[dataSet][dataset.Key] = dataset.Value;
+                            }
+                        }
+                    }
+
+                }
+            }
+            if (dic.Count == 0)
+            {
+                return;
+            }
+            // dic.Select(d => d.Value.Values.Sum())
+
+            string dicKeyTemp = "";
+            decimal dicSumTemp = 0m;
+            List<string> result = new List<string>();
+
+
+            foreach (var d in dic)
+            {
+                string str = d.Key;
+                var strDic = d.Value;
+
+                if ((decimal)d.Value.Values.Sum() > dicSumTemp)
+                {
+                    result.Clear();
+                    dicSumTemp = d.Value.Values.Sum();
+                    dicKeyTemp = d.Key;
+                    foreach (var s in d.Value)
+                    {
+                        string key = s.Key;
+                        result.Add(key);
+                    }
+                }
+
+            }
+
+            Console.WriteLine($"Data Set: {dicKeyTemp}, Total Size: {dicSumTemp}");
+            foreach (var item in result)
+            {
+                Console.WriteLine($"$.{item}");
+            }
+
+        }
+    }
+}
